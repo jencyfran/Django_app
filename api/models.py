be657a2acb6c827851ef3fcc742a2bf85e4aa042
JencyFrancisXavier <41000555+jencyfran@users.noreply.github.com>
2023-09-12 16:49:45 -0700

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# Location model to represent a location
 
 class Location(models.Model):
     name = models.CharField(unique=True, max_length=100)
@@ -7,11 +8,14 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+# Item model to represent an item
 
 class Item(models.Model):
     Name = models.CharField(max_length=150)
     date_added = models.DateField(auto_now_add=True)
     Location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
+    
+# String representation of the item
     def __str__(self):
         return self.Name
