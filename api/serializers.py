8e10a4ae6cdec83ff47dcab31d7f50f334caf738
JencyFrancisXavier <41000555+jencyfran@users.noreply.github.com>
2023-09-12 16:50:37 -0700

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from .models import Location, Item
 
+# Serializer for the Item model
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = ('__all__')
 
+
+# Serializer for the Location model
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
